7785c589b2e8284279fe11ffc67a315ecda8c09c
Anton Gorelkin <lucky42ru@mail.ru>
2025-10-14 22:18:34 +0200

    сделал секцию информация

--- food-app/src/pages/home-page/HomePage.tsx
+++ food-app/src/pages/home-page/HomePage.tsx
@@ -1,5 +1,12 @@
 import { HeroSection } from "../../components/hero-section/HeroSection";
+import { InfoSection } from "../../components/info-section/InfoSection";
 
 export function HomePage() {
-	return <HeroSection />;
+	return (
+		<>
+			<HeroSection />
+
+			<InfoSection />
+		</>
+	);
 }
